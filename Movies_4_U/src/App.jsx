2ef6e680c234b3ecf25e44d16db825eab5b35470
zakilaarabi <zakilaarabi@gmail.com>
2026-01-03 15:05:43 +0100

    creation, hero

--- Movies_4_U/src/App.jsx
+++ Movies_4_U/src/App.jsx
@@ -1,25 +1,24 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-function Card ({title}) {
-  const [hasLiked,setHasLiked] = useState(false);
-  return(
-    <div className='card' >
-    <h1>{title}</h1>
-    <button onClick={() =>setHasLiked(!hasLiked)}>{hasLiked ? '❤️' : '🩶'}</button>
-    </div>
-    
-  )
-}
+import Search from './components/Search'
 function App () {
 /* const [hasLiked,setHasLiked] = useState(initialState=false); */
 return (
-<div  className='card-container'>
+<div>
+  <main>
+    <div className='pattern'/>
+    <div className='wraper'>
+      <header>
+        <img src="..\hero-img.png" alt="" />
+        <h1 >
+          Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle 
+        </h1>
+      </header>
+      <Search/>
+    </div>
+  </main>
 
-<Card title={"Avatar"}/> 
-<Card title={"1917"}/>
-<Card title={"Revenant"}/>
 </div>
 )
 
