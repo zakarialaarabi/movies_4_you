3d11ad637f0726adb49cf69242666f3d44e4d7dc
zakilaarabi <zakilaarabi@gmail.com>
2026-01-07 12:47:42 +0100

    fix css

--- Movies_4_U/src/App.jsx
+++ Movies_4_U/src/App.jsx
@@ -7,17 +7,18 @@ function App () {
 return (
 <div>
   <main>
-    <div className='pattern'/>
-    <div className='wraper'>
-      <header>
-        <img src="..\hero-img.png" alt="" />
-        <h1 >
-          Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle 
-        </h1>
-      </header>
-      <Search/>
-    </div>
-  </main>
+  <div className="pattern" />
+  <header className="hero">
+    <img src="../hero-img.png" alt="" />
+    <h1>
+      Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle
+    </h1>
+  </header>
+
+  <div className="wrapper">
+    <Search />
+  </div>
+</main>
 
 </div>
 )
