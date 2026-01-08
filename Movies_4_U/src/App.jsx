1f0ad8a2613585d238c165c99c99927b2c5a2130
zakilaarabi <zakilaarabi@gmail.com>
2026-01-08 23:02:02 +0100

    fetch start

--- Movies_4_U/src/App.jsx
+++ Movies_4_U/src/App.jsx
@@ -1,9 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Search from './components/Search'
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_OPTIONS = {
+  method : 'GET',
+  headers : {
+    accept : 'application/json',
+    Authorization : `Bearer ${API_KEY}`
+  }
+}
 function App () {
-/* const [hasLiked,setHasLiked] = useState(initialState=false); */
+  const [searchTerm, setSearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const fetchMovies = async() =>{
+    try {
+      
+    } catch (error) {
+      console.error(`Error fetching movies : ${error}`)
+      setErrorMessage('Error fetching movies, Please try agian later.')
+    }
+  };
+
+  useEffect(()=>{
+
+  },[])
 return (
 <div>
   <main>
@@ -13,11 +35,18 @@ return (
     <h1>
       Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle
     </h1>
+    <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
   </header>
-
-  <div className="wrapper">
-    <Search />
+  <section className="all-movies">
+    <h2>All Movies</h2>
+    {errorMessage && <p className='text-red-500'>{errorMessage}</p>}
+  </section>
+  <div>
+    
   </div>
+  
+    
+
 </main>
 
 </div>
--- Movies_4_U/src/components/Search.jsx
+++ Movies_4_U/src/components/Search.jsx
@@ -1,8 +1,13 @@
 import React from "react"
-const Search = () => {
+const Search = ({searchTerm, setSearchTerm}) => {
     return (
-        <div className="text-white text-3xl">
-            Search
+        <div className="search">
+            <div>
+                <img src="search.svg" alt="search" />
+                <input type="text" placeholder="Search through thousands of movies" value={searchTerm} onChange={(e)=> setSearchTerm(e.target.value)}  />
+            </div>
+            
+
         </div>
     )
 
